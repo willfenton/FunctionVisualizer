da02b2008e141566850b277762b61264f347dc79
giancarlopernudisegura <pernudig@gmail.com>
2020-03-02 17:56:35 -0700

    basic line drawing with zoom

--- 2D/canvas.js
+++ 2D/canvas.js
@@ -273,58 +273,6 @@ var x_shift = 0.0;
 
 var scroll_zoom_multiplier = 0.1;
 
-function randomColor() {
-    // returns a random color value as HSL value
-    let vals = ["00", "ff", Math.floor(Math.random() * 256).toString(16)]
-    //shuffle
-    vals.sort(() => 0.5 - Math.random());
-    let color = `#${vals[0]}${vals[1]}${vals[2]}`;
-    return color;
-}
-
-function newPlot() {
-    // add DOM input
-    var options = {};
-    var opt = document.createElement("div");
-    var color = randomColor();
-    opt.className = "function-options";
-
-    // equation
-    options.equation = document.createElement("input");
-    options.equation.type = "text";
-    options.equation.placeholder = "type function here";
-    options.equation.className = "function-input ui";
-    opt.appendChild(options.equation);
-
-    // show/hide button
-    options.visibility = document.createElement("button");
-    options.visibility.className = "ui";
-    var icon = document.createElement("i");
-    icon.className = "fas fa-eye";
-    options.visibility.appendChild(icon);
-    opt.appendChild(options.visibility);
-
-    // remove button
-    options.remove = document.createElement("button");
-    options.remove.className = "ui";
-    var icon = document.createElement("i");
-    icon.className = "fas fa-minus";
-    options.remove.appendChild(icon);
-    opt.appendChild(options.remove);
-
-    // color picker
-    options.picker = document.createElement("input");
-    options.picker.type = "color";
-    options.picker.className = "ui";
-    options.picker.value = color;
-    opt.appendChild(options.picker);
-
-    document.querySelector("#plot-inputs").appendChild(opt);
-
-    // adds a plot to the list
-    plots.push(new Plot(options));
-}
-
 newPlot();
 
 // ---- canvas callback loop
--- 3D/canvas.js
+++ 3D/canvas.js
@@ -1,5 +1,6 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.z = 5;
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setClearColor('#000000');
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -8,8 +9,50 @@ document.body.appendChild(renderer.domElement);
 
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = (window.innerWidth, window.innerHeight);
-    camera.updateProjectMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 });
 
-renderer.render(scene, camera);
+let key;
+
+function keyboardPress(event) {
+    key = event.key;
+}
+
+function updateCameraPosition() {
+    if (key == 'w') {
+        camera.position.z += .1;
+    } else if (key == 's') {
+        camera.position.z -= .1;
+    }
+}
+
+function line() {
+    var points = [];
+    for (let i = -10; i < 10; i++) {
+        points.push(new THREE.Vector3(i, 0, 0));
+    }
+
+    var geometry = new THREE.BufferGeometry().setFromPoints(points);
+    var material = new THREE.LineBasicMaterial({color: 0xffffff});
+    var line = new THREE.Line(geometry, material);
+
+    scene.add(line);
+}
+
+line();
+
+var light = new THREE.PointLight( 0xffffff, 1, 100 );
+light.position.set(0, 5, 10);
+scene.add(light);
+
+scene.add(new THREE.AmbientLight(0xffffff));
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    updateCameraPosition();
+    key = undefined;
+}
+
+animate();
